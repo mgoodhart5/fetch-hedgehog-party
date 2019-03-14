4c47f964cb2fc2097c373b91e0c018f9e4d73adb
Mary Goodhart <mary.loves.pry@gmail.com>
2019-03-14 12:31:16 -0600

    Mary and Anten's paired hedgehog party class practice

--- public/index.js
+++ public/index.js
@@ -1,9 +1,12 @@
 const getHedgehogs = () => {
   $('#hedgehog-info').html('');
-
+// this is a get request for all the invited hedgehogs
   fetch(`https://hedgehog-party.herokuapp.com/api/v1/invites`)
+  //this turns the response into json if it was successful and returns another promise
     .then(response => response.json())
+    //if that was successful then hedgehogs are appended from the body of the response
     .then(hedgehogs => appendHedgehogs(hedgehogs))
+    // if the request above was not successful, this catch block will run and the error will be shown in the console
     .catch(error => console.error({ error }));
 };
 
@@ -29,12 +32,34 @@ const appendHedgehog = (hedgehog) => {
   `);
 };
 
-const addNewHedgehog = () => {
-  console.log("we are in the addNewHedgehog function");
+$(document).ready(function() {
+
+})
+
+const addNewHedgehog = (event) => {
+  event.preventDefault();
+  var nameHedgie = $("#name").val();
+  var hogletsHedgie = $("#hoglets").val();
+  var allergiesHedgie = $("#allergies").val();
+  fetch("https://hedgehog-party.herokuapp.com/api/v1/invites", {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify ({
+      name: nameHedgie,
+      hoglets: hogletsHedgie,
+      allergies: allergiesHedgie
+    })
+  }).then(response => response.json())
+  .catch(error => console.error({ error}))
 };
 
-const unInviteHedgehog = () => {
-  console.log("we are in the unInviteHedgehog function");
+const unInviteHedgehog = (event) => {
+  event.preventDefault();
+  var id = event.target.id
+  fetch('https://hedgehog-party.herokuapp.com/api/v1/invites/' + id, {
+    method: 'DELETE'
+  }).then(response => console.log("DESTROYED"))
+  .catch(error => console.error({ error }))
 };
 
 getHedgehogs();
